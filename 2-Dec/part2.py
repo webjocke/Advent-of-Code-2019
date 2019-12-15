433a77c058de950c3bbd50c53b861d5845658c6b
Joakim Johansson <jocke@somewhatcompetent.net>
2019-12-15 18:44:41 +0100

    Nice

--- 2-Dec/part2.py
+++ 2-Dec/part2.py
@@ -9,8 +9,8 @@ with open("i") as fp:
     index_one = 0
     index_two = 0
     while index_zero != 19690720:
-        index_one = all_combinations[combination_index][0] if flip else all_combinations[combination_index][1]
-        index_two = all_combinations[combination_index][1] if flip else all_combinations[combination_index][0]
+        index_one = all_combinations[combination_index][0]
+        index_two = all_combinations[combination_index][1]
 
         steps = original_steps.copy()
         steps[1] = index_one
@@ -18,16 +18,13 @@ with open("i") as fp:
 
         i = 0
         while (i <= len(steps)):
-            #print(i)
             if (steps[i] == 1): steps[steps[i+3]] = steps[steps[i+2]] + steps[steps[i+1]]
             elif (steps[i] == 2): steps[steps[i+3]] = steps[steps[i+2]] * steps[steps[i+1]]
             elif (steps[i] == 99): break
             i += 4
 
         index_zero = steps[0]
-        if flip == True:
-            combination_index += 1
-        flip = not flip
+        combination_index += 1
 
     print(index_zero, index_one, index_two)
     print(100*index_one+index_two)
