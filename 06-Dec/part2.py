008de2616531debcdc0f3cd3ac2f523b56d098bc
Joakim Johansson <webjocke@Joakims-MacBook-Pro.local>
2019-12-31 01:00:11 +0100

    Nice

--- 06-Dec/part2.py
+++ 06-Dec/part2.py
@@ -1,9 +1,30 @@
+from copy import deepcopy
 
-def f(o, a, d=0, r=0):
-    for b in a:
-        if b[0] == o:
-            r = d + 1 + f(b[1], a, d+1, r)
-    return r
+def f(orbit, all_o, path_to_here=[]):
+    path_to_here = deepcopy(path_to_here)
+    for orbit_local in all_o:
+        if orbit_local[1] == orbit:
+            path_to_here.append(orbit_local[0])
+            path_to_here = f(orbit_local[0], all_o, path_to_here)
+    return path_to_here
 
 with open("i") as i:
-    print (f("COM", [(x.split(')')[0], x.split(')')[1]) for x in i.read().split("\n")]))
+    lst = [(x.split(')')[0], x.split(')')[1]) for x in i.read().split("\n")]
+    yous = f("YOU", lst)
+    yous.reverse()
+    sans = f("SAN", lst)
+
+    orbit_steps = []
+    for san in sans:
+        orbit_steps.append(san)
+        if san in yous:
+            on = False
+            for you in yous:
+                if on:
+                    orbit_steps.append(you)
+                if you == san:
+                    on = True
+            break
+    print orbit_steps
+    print len(orbit_steps)-1
+    
